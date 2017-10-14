efa3e63e4720c4b0abfaa6accb52d8dbda8c984c
basnetroshan <basnet.roshan1991@gmail.com>
2017-10-14 20:50:46 +1000

    Method called to return bet to balance

--- src/src/Game.java
+++ src/src/Game.java
@@ -49,13 +49,13 @@ public class Game {
 		if (matches > 0) {
 			// Player should have his bet returned here because he wins
 			System.out.println("----GAME.JAVA: Player wins, his bet of " + bet + " should be returned to his balance here!");
- 			player.receiveWinnings(winnings);
+			
+			// The bellow method is called so that the bet amount can be added back to the player's balance on winning
+			player.receiveBetReturned(bet);
  			player.receiveWinnings(winnings);
 			System.out.println("----GAME.JAVA: Player received winnings of " + winnings);
 			System.out.println("----GAME.JAVA: Now player has balance of: " + player.getBalance());
 
-		if (matches > 0) {			
-			player.receiveWinnings(winnings);
 		}
         return winnings;		
 	}
